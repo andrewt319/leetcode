37a21cf488214721e4eae303c18345bbb418ff08
andrewt319 <andrew319t@gmail.com>
2025-11-14 18:21:59 -0800

    1091

---
+++ 1091_shortest_path_in_binary_matrix.py
@@ -0,0 +1,22 @@
+from collections import deque
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+
+        q = deque([(0, 0)])
+        sol = 0
+        n, m = len(grid), len(grid[0])
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                if r == n - 1 and c == m - 1:
+                    return sol + 1
+                
+                grid[r][c] = 1
+                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)]:
+                    if 0 <= r + dx < n and 0 <= c + dy < m and grid[r + dx][c + dy] == 0:
+                        grid[r + dx][c + dy] = 1
+                        q.append((r + dx, c + dy))
+            sol += 1
+        return -1
