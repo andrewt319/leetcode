cb9ef8c3ce600a5b6eadaf0ea49f09b2809f04d9
andrewt319 <andrew319t@gmail.com>
2025-11-22 11:05:21 -0800

    134

--- 134_gas_station.py
+++ 134_gas_station.py
@@ -1,17 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        currTank = totalTank = idx = 0
+        totalGas = totalCost = currGas = idx = 0
 
         for i in range(len(gas)):
-            # You need total gas to be > total cost in order for it to be possible to travel
-            # around, so should track this
-            totalTank += gas[i] - cost[i]
+            totalGas += gas[i]
+            totalCost += cost[i]
 
-            # Optimistically assume the next point after your tank is empty could be your start index.
-            # If gas[i] - cost[i] is positive at idx 1, 2 and 3, you start at the first one. 
-            currTank += gas[i] - cost[i]
-            if currTank < 0:
-                currTank = 0
-                idx = (i + 1) % len(gas)
+            currGas += gas[i] - cost[i]
+            if currGas < 0:
+                currGas = 0
+                idx = i + 1
         
-        return idx if totalTank >= 0 else -1
+        return idx if totalGas >= totalCost else -1
+
